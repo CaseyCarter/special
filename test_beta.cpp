d7968a83151702ae6a8ee04464e488916966d713
Casey Carter <Casey@Carter.net>
2018-02-21 10:55:40 -0800

    Rework beta test to also validate against the control implementation

--- test_beta.cpp
+++ test_beta.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <limits>
 #include <boost/array.hpp>
+#include <boost/math/special_functions.hpp>
 #include <boost/math/tools/precision.hpp>
 #include <boost/test/included/unit_test.hpp>
 #include "special.hpp"
@@ -13,38 +14,51 @@ namespace {
     template<class T>
     struct table_type { using type = T; };
 
-    template<class T, class F>
-    void test_beta(F f) {
+    template<class>
+    constexpr bool always_false = false;
+
+    template<class T>
+    constexpr auto control_fn = [](T x, T y) { return boost::math::beta(x, y); };
+
+    template<class T>
+    constexpr auto test_fn = [](auto x, T) { static_assert(always_false<decltype(x)>, "BOOM"); };
+    template<>
+    constexpr auto test_fn<float> = std::betaf;
+    template<>
+    constexpr auto test_fn<double> = std::beta;
+    template<>
+    constexpr auto test_fn<long double> = std::betal;
+
+    using types = boost::mpl::list<float, double, long double>;
+
+    BOOST_AUTO_TEST_CASE_TEMPLATE(test_beta, T, types) {
 #define SC_(X) static_cast<T>(BOOST_JOIN(X, L))
+#include "math/test/beta_small_data.ipp"
+#include "math/test/beta_med_data.ipp"
+#include "math/test/beta_exp_data.ipp"
+#undef SC_
+
         auto const eps = boost::math::tools::epsilon<T>();
 
-        {
-#include "math/test/beta_small_data.ipp"
-            for(auto const& datum : beta_small_data) {
-                BOOST_CHECK_CLOSE_FRACTION(f(datum[0], datum[1]), datum[2], 3 * eps);
-            }
+        for(auto const& datum : beta_small_data) {
+            auto const actual = test_fn<T>(datum[0], datum[1]);
+            BOOST_CHECK_EQUAL(actual, control_fn<T>(datum[0], datum[1]));
+            BOOST_CHECK_CLOSE_FRACTION(actual, datum[2], 3 * eps);
         }
 
-        {
-#include "math/test/beta_med_data.ipp"
-            for(auto const& datum : beta_med_data) {
-                BOOST_CHECK_CLOSE_FRACTION(f(datum[0], datum[1]), datum[2], 98 * eps);
-            }
+        for(auto const& datum : beta_med_data) {
+            auto const actual = test_fn<T>(datum[0], datum[1]);
+            BOOST_CHECK_EQUAL(actual, control_fn<T>(datum[0], datum[1]));
+            BOOST_CHECK_CLOSE_FRACTION(actual, datum[2], 98 * eps);
         }
 
-        {
-#include "math/test/beta_exp_data.ipp"
-            for(auto const& datum : beta_exp_data) {
-                BOOST_CHECK_CLOSE_FRACTION(f(datum[0], datum[1]), datum[2], 12 * eps);
-            }
+        for(auto const& datum : beta_exp_data) {
+            auto const actual = test_fn<T>(datum[0], datum[1]);
+            BOOST_CHECK_EQUAL(actual, control_fn<T>(datum[0], datum[1]));
+            BOOST_CHECK_CLOSE_FRACTION(actual, datum[2], 12 * eps);
         }
-#undef SC_
     }
 } // unnamed namespace
-
-BOOST_AUTO_TEST_CASE(beta)  { test_beta<double>(std::beta); }
-BOOST_AUTO_TEST_CASE(betaf) { test_beta<float>(std::betaf); }
-BOOST_AUTO_TEST_CASE(betal) { test_beta<long double>(std::betal); }
 
 int main(int argc, char *argv[]) {
     auto const result = boost::unit_test::unit_test_main(init_unit_test, argc, argv);
