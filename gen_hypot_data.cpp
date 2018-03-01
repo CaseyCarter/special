97e5995c4c3826edf96a5c8ac6842786a83d4902
Casey Carter <cacarter@microsoft.com>
2018-03-01 11:33:05 -0800

    Cleanup gen_hypot_data a bit

--- gen_hypot_data.cpp
+++ gen_hypot_data.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <cassert>
 #include <cmath>
 #include <cstring>
@@ -15,50 +13,46 @@ namespace {
     using big_float = boost::multiprecision::number<boost::multiprecision::backends::cpp_bin_float<1000,
         boost::multiprecision::backends::digit_base_2>>;
 
-    big_float hypot3(big_float x, big_float y, big_float z)
-        {
-        using std::swap;
+    big_float hypot3(double x, double y, double z) {
+        x = std::abs(x);
+        assert(!std::isinf(x) && !std::isnan(x));
 
-        x = abs(x);
-        assert(!isinf(x) && !isnan(x));
+        y = std::abs(y);
+        assert(!std::isinf(y) && !std::isnan(y));
 
-        y = abs(y);
-        assert(!isinf(y) && !isnan(y));
+        z = std::abs(z);
+        assert(!std::isinf(z) && !std::isnan(z));
 
-        z = abs(z);
-        assert(!isinf(z) && !isnan(z));
-
-        if (y > x)
-            {
-            swap(x, y);
-            }
-
-        if (z > x)
-            {
-            swap(x, z);
-            }
-
-        const auto frac_sq = [](const big_float num, const big_float denom)
-            {
-            const big_float result = num / denom;
-            return result * result;
-            };
-
-        return x * sqrt(1 + frac_sq(y, x) + frac_sq(z, x));
+        if (y > x) {
+            std::swap(x, y);
         }
 
-    void generate(double const low, std::string name) {
+        if (z > x) {
+            std::swap(x, z);
+        }
+
+        auto const frac_sq = [](double const num, double const denom) {
+            big_float result = num;
+            result /= denom;
+            result *= result;
+            return result;
+        };
+
+        return x * sqrt(1 + frac_sq(y, x) + frac_sq(z, x));
+    }
+
+    void generate(double const low, char const * const name) {
         constexpr auto n = 500;
         static auto engine = std::mt19937{};
 
-        double const high = low / boost::math::tools::epsilon<double>();
+        auto in_range = [low, high = low / boost::math::tools::epsilon<double>()](double const d) {
+            return low <= d && d < high;
+        };
 
         auto dist = std::uniform_int_distribution<unsigned long long>{};
         auto gen = [&] {
-            auto in_range = [=](double const d) {
-                return low <= d && d < high;
-            };
             double d;
+            // Choose uniformly from the set of double representations in [low, high)
             do {
                 auto const ull = dist(engine);
                 std::memcpy(&d, &ull, sizeof(ull));
@@ -66,12 +60,12 @@ namespace {
             return d;
         };
 
-        name = "hypot_" + name + "_data";
-        std::ofstream f{name + ".ipp"};
+        std::string basename = "hypot_" + std::string{name} + "_data";
+        std::ofstream f{basename + ".ipp"};
         assert(f);
         f.precision(std::numeric_limits<double>::digits10);
         f.flags(std::ios_base::fmtflags(std::ios_base::scientific));
-        f << "static const boost::array<boost::array<typename table_type<T>::type, 4>, " << n << "> " << name << " = {{\n";
+        f << "static const boost::array<boost::array<typename table_type<T>::type, 4>, " << n << "> " << basename << " = {{\n";
 
         for (auto i = 0; i < n; ++i) {
             double const d[3] = {gen(), gen(), gen()};
